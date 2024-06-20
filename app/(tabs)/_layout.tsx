7a4cfd4b1587d9e8200fbe4939e8787597512b33
Xecozz <94707887+Xecozz@users.noreply.github.com>
2024-06-20 14:58:58 +0200

    update

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -14,7 +14,7 @@ const TabIcon = ({ icon, color, name, focused }) => {
         className={`w-6 h-6`}
       />
       <Text
-        className={`${focused ? "font-semibold" : "font-regular"} text-xs`}
+        className={`${focused ? "font-semibold" : "font-pregular"} text-xs`}
         style={{ color: color }}
       >
         {name}
